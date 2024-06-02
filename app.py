e0086cd27190dfe5775eca5698a67877648267f8
modyrag <130484327+modyrag@users.noreply.github.com>
2024-06-02 19:59:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pickle
+import os
 
 # Load the SentimentIntensityAnalyzer object from the pickle file
 with open('sentiment_analyzer.pkl', 'rb') as f:
@@ -34,4 +35,6 @@ def analyze_sentiment_route():
     return jsonify({"sentiment": sentiment_label})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Get the port number from the environment variable, default to 5000 if not set
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
